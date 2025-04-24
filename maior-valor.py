d7930f456fcdeedc73a7a5708d6db8e0a0959b50
SeuNomeDeUsuario <seuemail@example.com>
2025-04-24 11:34:00 -0300

    Primeiro commit do projeto

---
+++ maior-valor.py
@@ -0,0 +1,8 @@
+num_1 = int(input("Digite primeiro número:"))
+num_2 = int(input("Digite o segundo número:"))
+if num_1 > num_2:
+    print("Número um é maior que o número dois!")
+elif num_2 > num_1:
+    print("Número dois é maior que o número um!")
+else:
+    print("Eles são números iguais!")
