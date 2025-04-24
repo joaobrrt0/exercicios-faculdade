36cda37b0d8068d09914f25c331cd8c414983a0f
SeuNomeDeUsuario <seuemail@example.com>
2025-04-24 14:14:43 -0300

    Adiciona exercício que ordena 3 numeros em ordem crescente

---
+++ ordem-cresc.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Digite o primeiro número:"))
+num_2 = int(input("Digite o segundo número:"))
+num_3 = int(input("Digite o terceiro número:"))
+
+numeros = [num_1,num_2,num_3]
+
+numeros.sort()
+
+print("Números na sua ordem crescente:", numeros)
