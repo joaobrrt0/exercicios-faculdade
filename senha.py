24241cc4d3e689efcb4f9d51f1e7f9f80bd29b91
SeuNomeDeUsuario <seuemail@example.com>
2025-04-24 12:04:13 -0300

    Adiciona script de senha

--- senha.py
+++ senha.py
@@ -1,5 +1,14 @@
-senha = int(input("Digite sua senha:"))
-if senha == 1234:
+# Solicita ao usuário criar uma senha inicial
+senha_inicial = input("Crie uma senha inicial: ")
+
+# Imprime mensagem de confirmação
+print("Senha criada com sucesso!")
+
+# Solicita ao usuário digitar a senha para verificação
+senha_digitada = input("Digite sua senha para acessar: ")
+
+# Verifica se a senha digitada é igual à senha criada
+if senha_digitada == senha_inicial:
     print("ACESSO PERMITIDO")
 else:
     print("ACESSO NEGADO")
